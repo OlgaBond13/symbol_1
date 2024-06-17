20b3cf9535f94412711a57b69a4b9841e999436b
olga_bond <bondarch-olga@yandex.ru>
2024-06-18 00:33:46 +0700

    added info

--- Program.cs
+++ Program.cs
@@ -6,4 +6,28 @@
 //  лучше обойтись исключительно массивами.
 
 
+string[] array1 = {"Heiio","2","world",":-0","Next","1227","2134","333"};
+string[] array2 = new string[array1.Length];
 
+void SecondArray(string[] array1, string[] array2)
+{
+    int count=0;
+    for(int i=0; i<array1.Length; i++)
+    {
+        if(array1[i].Length<=3)
+        {
+            array2[count]=array1[i];
+            count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for(int i=0; i<array.Length; i++)
+    {
+        Console.Write($"{array[i]}" +" ");
+    }
+    Console.WriteLine();
+}
+SecondArray(array1,array2);
+PrintArray(array2);
--- obj/Debug/net8.0/symbol_1.AssemblyInfo.cs
+++ obj/Debug/net8.0/symbol_1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("symbol_1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5367907f4c9de2f6ffb6cba8bd84a549522543f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c9dde0d70b2ed41f3e68c71c61fd1c0826f6651")]
 [assembly: System.Reflection.AssemblyProductAttribute("symbol_1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("symbol_1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
